2749b48f253b4e2e68c67db3416752fe51b9954a
TheOrbMaverick <enangita@gmail.com>
2024-07-23 22:08:34 +0100

    understanding caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -16,16 +16,14 @@ class FIFOCache(BaseCaching):
         self.last_key = None
 
     def put(self, key, item):
-        """Add an item in the cache"""
-        self.cache_data[key] = item
-        if key is None or item is None:
-            return
-        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-            if self.last_key is not None:
-                print(f"DISCARD: {self.last_key}")
-                del self.cache_data[self.last_key]
-        self.cache_data[key] = item
-        self.last_key = key
+        """ Add an item in the cache """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                if self.last_key is not None:
+                    print(f"DISCARD: {self.last_key}")
+                    del self.cache_data[self.last_key]
+            self.cache_data[key] = item
+            self.last_key = key
 
     def get(self, key):
         """ Get an item by key """
