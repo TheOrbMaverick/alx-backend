f7df7dd63a029ef2cb5080ae53572070afc42bc5
TheOrbMaverick <enangita@gmail.com>
2024-09-22 15:11:40 +0100

    translations to .mo

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,64 +1,52 @@
 #!/usr/bin/env python3
 """
-A basic Flask application demonstrating i18n (internationalization) support using Flask-Babel.
-
-This app renders a homepage with translated content based on the user's locale preferences.
-It supports both English and French languages.
+Basic Flask app with a single route and Babel for i18n.
 """
 
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
+
 class Config:
     """
-    Configuration class for the Flask application.
-    
-    Attributes:
-        LANGUAGES (list): Supported languages for the application.
-        BABEL_DEFAULT_LOCALE (str): Default locale (language) for the app.
-        BABEL_DEFAULT_TIMEZONE (str): Default timezone for the app.
+    Configuration class for Flask app.
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 
-# Initialize Babel for internationalization
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
     """
-    Select the best matching locale based on the 'Accept-Language' header sent by the user's browser.
-    
-    This function is automatically called by Flask-Babel to determine the appropriate language for
-    the current request.
-
-    Returns:
-        str: The best matching language from the supported languages (Config.LANGUAGES).
+    Determine the best match with supported languages.
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
 def index():
     """
-    Render the homepage with localized strings for the title and header.
-
-    This route uses the Babel `gettext` function (aliased as `_`) to retrieve translated strings
-    for the title ('home_title') and header ('home_header') of the page, based on the current locale.
+    _
     
-    Returns:
-        str: Rendered HTML content of the homepage.
+    or getext function to get translated strings for the title
+    and header based on the selected language.
+
+    Route for the home page.
     """
     return render_template(
         '3-index.html',
         home_title=_("home_title"),
         home_header=_("home_header")
-    )
+        )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
